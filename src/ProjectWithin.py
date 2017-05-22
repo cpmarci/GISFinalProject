b41f9f7001461ffc8d7330e00eb0fa970691e2d4
Kyle Wong <kdwong1211@gmail.com>
2017-05-22 15:19:56 -0500

    time within
    added timewithin

--- src/ProjectWithin.py
+++ src/ProjectWithin.py
@@ -2,21 +2,20 @@
 @author: Kyle Wong
 Test implemenation for project
 '''
-
+import time
 import psycopg2
-
-conn = psycopg2.connect(dbname = 'postgres', host= 'localhost', port= 5432, user = 'postgres',password= 'jeep1999')
+start = time.time()
+conn = psycopg2.connect(dbname = 'postgres', host= 'localhost', port= 5432, user = 'postgres',password= 'dragon01')
 cur = conn.cursor()
-testintersect = '''select a.gid,b.gid,ST_DWITHIN(a.geom, b.geom, 1000), ST_asTEXT(a.geom)
-from testdata a, testpoly b '''
-projintersect = '''select a.pid,b.pyid,ST_DWITHIN(a.geom, b.geom, 1000)
+testintersect = '''select a.gid,b.gid,ST_DWITHIN(a.geom, b.geom, 1000), a.point, a.time, b.polygon, b.time, ST_asTEXT(a.geom)
+from testpointtime a, testpolytime b '''
+projintersect = '''select a.pid,b.pyid,ST_DWITHIN(a.geom, b.geom, 1000), a.prefnum, a.ptime, b.pyref, b.pytime, ST_asText(a.geom)
 from points500 a, poly10 b '''
-cur.execute(testintersect)
+cur.execute(projintersect)
 conn.commit()
-cur.execute(testintersect)
+cur.execute(projintersect)
 parseArray = cur.fetchall()
 
-#print(parseArray)
 #print(parseArray)
 container = list()
 print(parseArray[2])
@@ -29,16 +28,14 @@ for n in range(len(parseArray)):
     if holder[2] == True:
         ##print(holder[2])
         counter = counter + 1
-        writeline = str(holder[0])+":"+str(holder[1]) + "\n"
+        writeline = str(holder[3])+"-"+str(holder[4]) +":" + str(holder[5]) +"-"+str(holder[6]) + "\n"
         f.write(writeline)
-        container.append(str(holder[0])+":"+str(holder[1]))
-        holderTemp = str(holder[3])
-        print("'" + holderTemp + "'")
-        tempOne = "INSERT INTO testwithin (geom) VALUES (ST_GeomFromText("+ "'" + holderTemp + "'" + ", 4269))"#, (holderTemp))
-        print(tempOne)
-        cur.execute(tempOne)
-        conn.commit()
+        container.append(str(holder[3])+"-"+str(holder[4]) +":" + str(holder[5]) +"-"+str(holder[6]))
+    
     
 print(container)
 print(counter)
+end = time.time()
+
+print(end- start)
 f.close()
