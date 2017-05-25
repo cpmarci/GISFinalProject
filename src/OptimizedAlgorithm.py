3dabe50e02476ed1ed8d4a71e287381959543510
Kyle Wong <kdwong1211@gmail.com>
2017-05-25 15:37:32 -0500

    changes
    running big

--- src/OptimizedAlgorithm.py
+++ src/OptimizedAlgorithm.py
@@ -21,18 +21,24 @@ poly10 = 30
 testintersect = ('''select a.gid,b.gid,a.point, a.time, b.polygon, b.time, ST_asTEXT(a.geom)
 from testpointtime a, testpolytime b WHERE a.gid={0} and a.time > b.time and ST_Intersects(a.geom, b.geom) = True '''. format(first))
 
-projintersect = '''select a.pid,b.pyid,ST_Intersects(a.geom, b.geom), a.prefnum, a.ptime, b.pyref, b.pytime, ST_asText(a.geom)
-from points500 a, poly10 b where a.ptime < b.pytime'''
+projintersect = ('''select a.pid,b.pyid,ST_Intersects(a.geom, b.geom), a.prefnum, a.ptime, b.pyref, b.pytime, ST_asText(a.geom)
+from points500 a, poly10 b WHERE a.pid={0} and a.ptime > b.pytime and ST_Intersects(a.geom, b.geom) = true'''. format(first))
+
+projintersect3 = ('''select a.pid,b.pyid,ST_Intersects(a.geom, b.geom), a.prefnum, a.ptime, b.pyref, b.pytime, ST_asText(a.geom)
+from points500 a, poly10 b where a.pid{0} a.ptime > b.pytime and ST_Intersects(a.geom, b.geom) = true'''. format(first))
+
 
 f = open("C:\Data\ProjectOutputIntersect.txt","w+")
 container = list()
 counter = 0
-for i in range(testdata):
+for i in range(points500):
     testintersect2 = ('''select a.gid,b.gid,a.point, a.time, b.polygon, b.time, ST_asTEXT(a.geom)
     from testpointtime a, testpolytime b WHERE a.gid={0} and a.time >= b.time and ST_Intersects(a.geom, b.geom) = True '''. format(i))
-    cur.execute(testintersect2)
+    projintersect2 = ('''select a.pid,b.pyid, a.prefnum, a.ptime, b.pyref, b.pytime, ST_asText(a.geom)
+    from points500 a, poly10 b WHERE a.pid={0} and a.ptime >= b.pytime and ST_Intersects(a.geom, b.geom) = True'''. format(i))
+    cur.execute(projintersect2)
     conn.commit()
-    cur.execute(testintersect2)
+    cur.execute(projintersect2)
     parseArray = cur.fetchall()
     
     
@@ -43,10 +49,27 @@ for i in range(testdata):
         writeline = str(holder[2])+"-"+str(holder[3]) +":" + str(holder[4]) +"-"+str(holder[5]) + "\n"
         f.write(writeline)
         print(str(holder[2])+"-"+str(holder[3]) +":" + str(holder[4]) +"-"+str(holder[5]))
-        #print(type(parseArray[0]))
-end = time.time()
+
 
 print(counter)
-
+A = open("C:\Data\ProjectOutputWithin.txt","w+")
+counter2 = 0
+for k in range(testdata):
+    testwithint2 = ('''select a.gid,b.gid,a.point, a.time, b.polygon, b.time, ST_asTEXT(a.geom)
+    from testpointtime a, testpolytime b WHERE a.gid={0} and a.time >= b.time and ST_DWITHIN(a.geom, b.geom, 1000) = True '''. format(k))
+    cur.execute(testwithint2)
+    conn.commit()
+    cur.execute(testwithint2)
+    parseArray2 = cur.fetchall()
+    if len(parseArray2) != 0:
+        # print(str(parseArray[2])+"-"(+str(parseArray[3]) +":" + str(parseArray[4]) +"-"+str(parseArray[5]))
+        holder2 = parseArray2[0]
+        counter2 = counter2 + 1
+        writeline2 = str(holder2[2])+"-"+str(holder2[3]) +":" + str(holder2[4]) +"-"+str(holder2[5]) + "\n"
+        A.write(writeline)
+        #print(str(holder2[2])+"-"+str(holder2[3]) +":" + str(holder2[4]) +"-"+str(holder2[5]))
+        #print(type(parseArray[0]))
+end = time.time()
+print(counter2)
 print(end- start)
 f.close()
--- src/ProjectIntersect.py
+++ src/ProjectIntersect.py
@@ -8,20 +8,20 @@ start = time.time()
 conn = psycopg2.connect(dbname = 'postgres', host= 'localhost', port= 5432, user = 'postgres',password= 'dragon01')
 cur = conn.cursor()
 testintersect = '''select a.gid,b.gid,ST_Intersects(a.geom, b.geom),a.point, a.time, b.polygon, b.time, ST_asTEXT(a.geom)
-from testpointtime a, testpolytime b '''
+from testpointtime a, testpolytime b WHERE a.time > b.time and ST_Intersects(a.geom, b.geom) = true'''
 
 projintersect = '''select a.pid,b.pyid,ST_Intersects(a.geom, b.geom), a.prefnum, a.ptime, b.pyref, b.pytime, ST_asText(a.geom)
-from points500 a, poly10 b where a.ptime < b.pytime'''
-cur.execute(testintersect)
+from points500 a, poly10 b where a.ptime > b.pytime and ST_Intersects(a.geom, b.geom) = true'''
+cur.execute(projintersect)
 conn.commit()
-cur.execute(testintersect)
+cur.execute(projintersect)
 parseArray = cur.fetchall()
 
 #print(parseArray)
 container = list()
-print(parseArray[2])
-first = parseArray[2]
-print(first[3])
+#print(parseArray[2])
+#first = parseArray[2]
+#print(first[3])
 f = open("C:\Data\ProjectOutputIntersect.txt","w+")
 counter = 0
 for n in range(len(parseArray)):
@@ -34,7 +34,7 @@ for n in range(len(parseArray)):
         container.append(str(holder[3])+"-"+str(holder[4]) +":" + str(holder[5]) +"-"+str(holder[6]))
     
     
-print(container)
+#print(container)
 print(counter)
 end = time.time()
 
