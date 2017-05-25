4ffa4d5bf15addbd8dfb4725e71938b8e35b6381
Kyle Wong <kdwong1211@gmail.com>
2017-05-25 12:59:16 -0500

    testpoint format

--- src/OptimizedAlgorithm.py
+++ src/OptimizedAlgorithm.py
@@ -12,13 +12,14 @@ start = time.time()
 conn = psycopg2.connect(dbname = 'postgres', host= 'localhost', port= 5432, user = 'postgres',password= 'dragon01')
 cur = conn.cursor()
 
+first = 1
 
-
-testintersect = '''select a.gid,b.gid,ST_Intersects(a.geom, b.geom),a.point, a.time, b.polygon, b.time, ST_asTEXT(a.geom)
-from testpointtime a, testpolytime b '''
+testintersect = ('''select a.gid,b.gid,ST_Intersects(a.geom, b.geom),a.point, a.time, b.polygon, b.time, ST_asTEXT(a.geom)
+from testpointtime a, testpolytime b WHERE a.gid={0} '''. format(first))
 
 projintersect = '''select a.pid,b.pyid,ST_Intersects(a.geom, b.geom), a.prefnum, a.ptime, b.pyref, b.pytime, ST_asText(a.geom)
 from points500 a, poly10 b where a.ptime < b.pytime'''
+
 cur.execute(testintersect)
 conn.commit()
 cur.execute(testintersect)
@@ -26,9 +27,6 @@ parseArray = cur.fetchall()
 
 #print(parseArray)
 container = list()
-print(parseArray[2])
-first = parseArray[2]
-print(first[3])
 f = open("C:\Data\ProjectOutputIntersect.txt","w+")
 counter = 0
 for n in range(len(parseArray)):
