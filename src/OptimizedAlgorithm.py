1fbca7903bfae8ed4255394de0a840f95eb7185e
Kyle Wong <kdwong1211@gmail.com>
2017-05-25 13:39:54 -0500

    update

--- src/OptimizedAlgorithm.py
+++ src/OptimizedAlgorithm.py
@@ -14,35 +14,32 @@ conn = psycopg2.connect(dbname = 'postgres', host= 'localhost', port= 5432, user
 cur = conn.cursor()
 
 first = 5
-
-testintersect = ('''select a.gid,b.gid,ST_Intersects(a.geom, b.geom),a.point, a.time, b.polygon, b.time, ST_asTEXT(a.geom)
-from testpointtime a, testpolytime b WHERE a.gid={0} and a.time > b.time '''. format(first))
+testdata = 25
+testpoly = 8
+points500 =39289
+poly10 = 30
+testintersect = ('''select a.gid,b.gid,a.point, a.time, b.polygon, b.time, ST_asTEXT(a.geom)
+from testpointtime a, testpolytime b WHERE a.gid={0} and a.time > b.time and ST_Intersects(a.geom, b.geom) = True '''. format(first))
 
 projintersect = '''select a.pid,b.pyid,ST_Intersects(a.geom, b.geom), a.prefnum, a.ptime, b.pyref, b.pytime, ST_asText(a.geom)
 from points500 a, poly10 b where a.ptime < b.pytime'''
 
-cur.execute(testintersect)
-conn.commit()
-cur.execute(testintersect)
-parseArray = cur.fetchall()
 
-#print(parseArray)
-container = list()
-f = open("C:\Data\ProjectOutputIntersect.txt","w+")
-counter = 0
-for n in range(len(parseArray)):
-    holder = parseArray[n]
-    if holder[2] == True:
-        ##print(holder[2])
-        counter = counter + 1
-        writeline = str(holder[3])+"-"+str(holder[4]) +":" + str(holder[5]) +"-"+str(holder[6]) + "\n"
-        f.write(writeline)
-        container.append(str(holder[3])+"-"+str(holder[4]) +":" + str(holder[5]) +"-"+str(holder[6]))
+
+for i in range(testdata):
+    testintersect2 = ('''select a.gid,b.gid,a.point, a.time, b.polygon, b.time, ST_asTEXT(a.geom)
+    from testpointtime a, testpolytime b WHERE a.gid={0} and a.time > b.time and ST_Intersects(a.geom, b.geom) = True '''. format(i))
     
     
-print(container)
-print(counter)
+
+    cur.execute(testintersect2)
+    
+    conn.commit()
+    cur.execute(testintersect2)
+    allReadable = cur.fetchall()
+    print(allReadable)
 end = time.time()
 
+
+
 print(end- start)
-f.close()
