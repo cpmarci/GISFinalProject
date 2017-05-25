3f5c51315ca139836104d62dfa47af8c8936f9d9
Kyle Wong <kdwong1211@gmail.com>
2017-05-25 12:29:44 -0500

    New file intro

---
+++ src/OptimizedAlgorithm.py
@@ -0,0 +1,42 @@
+'''
+@author: Kyle Wong
+Test implemenation for project
+'''
+import time
+import psycopg2
+start = time.time()
+conn = psycopg2.connect(dbname = 'postgres', host= 'localhost', port= 5432, user = 'postgres',password= 'dragon01')
+cur = conn.cursor()
+testintersect = '''select a.gid,b.gid,ST_Intersects(a.geom, b.geom),a.point, a.time, b.polygon, b.time, ST_asTEXT(a.geom)
+from testpointtime a, testpolytime b '''
+
+projintersect = '''select a.pid,b.pyid,ST_Intersects(a.geom, b.geom), a.prefnum, a.ptime, b.pyref, b.pytime, ST_asText(a.geom)
+from points500 a, poly10 b where a.ptime < b.pytime'''
+cur.execute(projintersect)
+conn.commit()
+cur.execute(projintersect)
+parseArray = cur.fetchall()
+
+#print(parseArray)
+container = list()
+print(parseArray[2])
+first = parseArray[2]
+print(first[3])
+f = open("C:\Data\ProjectOutputIntersect.txt","w+")
+counter = 0
+for n in range(len(parseArray)):
+    holder = parseArray[n]
+    if holder[2] == True:
+        ##print(holder[2])
+        counter = counter + 1
+        writeline = str(holder[3])+"-"+str(holder[4]) +":" + str(holder[5]) +"-"+str(holder[6]) + "\n"
+        f.write(writeline)
+        container.append(str(holder[3])+"-"+str(holder[4]) +":" + str(holder[5]) +"-"+str(holder[6]))
+    
+    
+print(container)
+print(counter)
+end = time.time()
+
+print(end- start)
+f.close()
