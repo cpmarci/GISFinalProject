ff6a65783242fb9d7f62ef804348f28a25b196f4
Kyle Wong <kdwong1211@gmail.com>
2017-05-25 13:56:01 -0500

    needs splicing
    here

--- src/OptimizedAlgorithm.py
+++ src/OptimizedAlgorithm.py
@@ -25,19 +25,19 @@ projintersect = '''select a.pid,b.pyid,ST_Intersects(a.geom, b.geom), a.prefnum,
 from points500 a, poly10 b where a.ptime < b.pytime'''
 
 
-
+container = list()
 for i in range(testdata):
     testintersect2 = ('''select a.gid,b.gid,a.point, a.time, b.polygon, b.time, ST_asTEXT(a.geom)
     from testpointtime a, testpolytime b WHERE a.gid={0} and a.time > b.time and ST_Intersects(a.geom, b.geom) = True '''. format(i))
-    
-    
-
     cur.execute(testintersect2)
-    
     conn.commit()
     cur.execute(testintersect2)
-    allReadable = cur.fetchall()
-    print(allReadable)
+    parseArray = cur.fetchall()
+    print(parseArray)
+    
+    if len(parseArray) != 0:
+            print(str(parseArray[1])+"-"+str(parseArray[2]))
+    
 end = time.time()
 
 
